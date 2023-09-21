35882411cdebb2720781dfb1c9e8de9fd0b45407
nilwandjg <nilwandjg@yahoo.com.br>
2023-09-21 05:53:49 -0300

    alternando os estágios do jogo

--- src/App.js
+++ src/App.js
@@ -23,13 +23,26 @@ function App() {
   const [gameStage, setGameStage] = useState(stages[0].name);
   const [words] = useState(wordsList);
 
-  console.log(words)
+  // Start
+  const startGame = () => {
+    setGameStage(stages[1].name)
+  }
+
+  // Verificação de letras digitadas
+  const verifyLetter = () => {
+    setGameStage(stages[2].name)
+  }
+
+  // Reiniciar o jogo
+  const retry = () => {
+    setGameStage(stages[0].name)
+  }
 
   return (
     <div className="App">
-      {gameStage === "start" && <StartScreen />}
-      {gameStage === "game" && <Game />}
-      {gameStage === "end" && <GameOver />}
+      {gameStage === "start" && <StartScreen startGame={startGame} />}
+      {gameStage === "game" && <Game verifyLetter={verifyLetter} />}
+      {gameStage === "end" && <GameOver retry={retry}/>}
     </div>
   );
 }
